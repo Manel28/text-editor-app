4f98abe13f7ef432f598ea5b5c8a1e9c634cd92a
Manel-smll <rachasarasmail@gmail.com>
2025-05-05 11:59:52 +0200

    editer le contenu avec ReactQuill

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 import '../App.css'
 
 function TextEditor({ content, onChange }) {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ color: [] }, { background: [] }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link'],
+      ['clean']
+    ]
+  }
+
   return (
-    <textarea
-      className="text-editor"
-      value={content}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder="Commence à écrire ici..."
-    />
+    <div className="rich-editor">
+      <ReactQuill
+        theme="snow"
+        value={content}
+        onChange={onChange}
+        modules={modules}
+        placeholder="Commence à écrire ici..."
+      />
+    </div>
   )
 }
 
